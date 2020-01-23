c21c840f1dda134bf0d36e367ad72a1dfe56e580
Ilya Zarubin <zarubin@wiso.uni-koeln.de>
2020-01-23 13:12:02 +0100

    Deleting unnecessary comments before modifying testing verison of cpp-pgas function

--- tests/pgas_testing_rcpp_versions.cpp
+++ tests/pgas_testing_rcpp_versions.cpp
@@ -477,13 +477,6 @@ arma::mat cbpf_as_c2_full(const int& N,
   x_out.col(4) = xa5.row(b_draw).t();
   x_out.col(5) = xa6.row(b_draw).t();
   return (x_out);
-  // return (List::create(xa1.row(b_draw),
-  //                     xa2.row(b_draw),
-  //                     xa3.row(b_draw),
-  //                     xa4.row(b_draw),
-  //                     xa5.row(b_draw),
-  //                     xa6.row(b_draw)));
-  // return(List::create(w, xa1, xa2, xa3, xa4, xa5, xa6));
 }
 
 // [[Rcpp::export]]
@@ -757,13 +750,6 @@ arma::mat cbpf_as_c3_full(const int& N,
   x_out.col(4) = xa5.row(b_draw).t();
   x_out.col(5) = xa6.row(b_draw).t();
   return (x_out);
-  // return (List::create(xa1.row(b_draw),
-  //                     xa2.row(b_draw),
-  //                     xa3.row(b_draw),
-  //                     xa4.row(b_draw),
-  //                     xa5.row(b_draw),
-  //                     xa6.row(b_draw)));
-  // return(List::create(w, xa1, xa2, xa3, xa4, xa5, xa6));
 }
 
 //[[Rcpp::export]]
@@ -781,19 +767,10 @@ List pgas2_full(const int& N,
                 const vec& priors,
                 const List& par_init,
                 const vec& traj_init) {
-  // par_true = NULL,
-  // filtering = TRUE,
-  // num_plots_states
   int D = par_init.size();
   // Initialize result containers:
   vec w(N, fill::zeros);
   mat Xa(TT*D, MM, fill::zeros);
-  // mat Xa1(MM, TT, fill::zeros);
-  // mat Xa2(MM, TT, fill::zeros);
-  // mat Xa3(MM, TT, fill::zeros);
-  // mat Xa4(MM, TT, fill::zeros);
-  // mat Xa5(MM, TT, fill::zeros);
-  // mat Xa6(MM, TT, fill::zeros);
 
   mat phi_x(D, MM, fill::zeros);
   mat sig_sq_x(D, MM, fill::zeros);
@@ -859,18 +836,6 @@ List pgas2_full(const int& N,
   mat prior_V_xa6 = diagmat(temp_vec_col);
   // Initialize states
   // I. Set states to deterministic starting values
-  // temp_vec_row.fill(traj_init(0));
-  // Xa1.row(0) = temp_vec_row;
-  // temp_vec_row.fill(traj_init(1));
-  // Xa2.row(0) = temp_vec_row;
-  // temp_vec_row.fill(traj_init(2));
-  // Xa3.row(0) = temp_vec_row;
-  // temp_vec_row.fill(traj_init(3));
-  // Xa4.row(0) = temp_vec_row;
-  // temp_vec_row.fill(traj_init(4));
-  // Xa5.row(0) = temp_vec_row;
-  // temp_vec_row.fill(traj_init(5));
-  // Xa6.row(0) = temp_vec_row;
   temp_vec_col.set_size(TT);
   for (int d = 1; d < D+1; ++d) {
     temp_vec_col.fill(traj_init(d-1));
@@ -963,7 +928,7 @@ List pgas2_full(const int& N,
       mu_xa(d, 0) = Omega_xa(d, 0) * (trans(Z.cols(id_zet(d), id_zet(d + 1) - 1)) * temp_vec_col)/sig_sq_x(d, m);
       // // mu_xa(d, 0) = mvrnorm_c(mu_xa(d, 0), Omega_xa(d, 0));
       mu_xa(d, 0) = mvnrnd(mu_xa(d, 0), Omega_xa(d, 0));
-      phi_x(d, m) =  (mu_xa(d, 0))(0);
+      phi_x(d, m) = (mu_xa(d, 0))(0);
       bet.submat(id_bet(d), m, id_bet(d + 1) - 1, m) =  (mu_xa(d, 0)).subvec(1, (D - 1));
     }
   //   // double digits = 1000;
@@ -1037,68 +1002,3 @@ List pgas2_full(const int& N,
   // Rcpp::Named("xtraj")  = all_traj));
   return(List::create(1));
 }
-// double m1 = 0;
-// double m2 = 1;
-// return(List::create(Xa.submat(0, m1, TT - 1, m1).t(),
-//                     Xa.submat(TT, m1, TT*2 - 1, m1).t(),
-//                     Xa.submat(TT*2, m1, TT*3 - 1, m1).t(),
-//                     Xa.submat(TT*3, m1, TT*4 - 1, m1).t(),
-//                     Xa.submat(TT*4, m1, TT*5 - 1, m1).t(),
-//                     Xa.submat(TT*5, m1, TT*6 - 1, m1).t(),
-//                     // sig_sq_x.row(0).subvec(0, 3),
-//                     // sig_sq_x.row(1).subvec(0, 3),
-//                     // sig_sq_x.row(2).subvec(0, 3),
-//                     // sig_sq_x.row(3).subvec(0, 3),
-//                     // sig_sq_x.row(4).subvec(0, 3),
-//                     // sig_sq_x.row(5).subvec(0, 3),
-//                     // Omega_xa(0, 0),
-//                     phi_x(0, m2), bet.submat(id_bet(0), m2, id_bet(0 + 1) - 1, m2), //mu_xa(0, 0),
-//                     // Omega_xa(1, 0),
-//                     phi_x(1, m2), bet.submat(id_bet(1), m2, id_bet(1 + 1) - 1, m2), //mu_xa(1, 0),
-//                     // Omega_xa(2, 0),
-//                     phi_x(2, m2), bet.submat(id_bet(2), m2, id_bet(2 + 1) - 1, m2), //mu_xa(2, 0),
-//                     // Omega_xa(3, 0),
-//                     phi_x(3, m2), bet.submat(id_bet(3), m2, id_bet(3 + 1) - 1, m2), //mu_xa(3, 0),
-//                     // Omega_xa(4, 0),
-//                     phi_x(4, m2), bet.submat(id_bet(4), m2, id_bet(4 + 1) - 1, m2), //mu_xa(4, 0),
-//                     // Omega_xa(5, 0),
-//                     phi_x(5, m2), bet.submat(id_bet(5), m2, id_bet(5 + 1) - 1, m2)));
-// double m =0;
-// return(List::create(Xa.submat(0, m, TT - 1, m).t()(Xa.submat(0, m, TT - 1, m).t(), 2), 4));
-// return(List::create(sig_sq_x.row(0).subvec(0, 3),
-//                     Omega_xa(0, 0), mu_xa(0, 0),
-//                     sig_sq_x.row(1).subvec(0, 3),
-//                     Omega_xa(1, 0), mu_xa(1, 0),
-//                     sig_sq_x.row(2).subvec(0, 3),
-//                     Omega_xa(2, 0), mu_xa(2, 0),
-//                     sig_sq_x.row(3).subvec(0, 3),
-//                     Omega_xa(3, 0), mu_xa(3, 0),
-//                     sig_sq_x.row(4).subvec(0, 3),
-//                     Omega_xa(4, 0), mu_xa(4, 0),
-//                     sig_sq_x.row(5).subvec(0, 3),
-//                     Omega_xa(5, 0), mu_xa(5, 0)));
-// double m = 1;
-// double d = 1;
-// return(List::create(Xa.submat(TT*0 + 1, m - 1, TT*(0 + 1) - 1, m - 1), Xa.submat(TT*d + 1, m - 1, TT*(d + 1) - 1, m - 1),
-//                     Z.submat(0, id_zet(0) + 1, TT - 2, id_zet(0 + 1) - 1),
-//                     Z.submat(0, id_zet(d) + 1, TT - 2, id_zet(d + 1) - 1),
-//                     Z,
-//                     phi_x(d, m - 1),
-//                     bet.submat(id_bet(d),  m - 1, id_bet(d + 1) - 1,  m - 1)));
-// return(List::create(Za1.submat(1, 0, TT - 1, dim_pars(0) - 3), Za1, Z));
-// return(List::create((Xa1.submat(0, 1, 0, TT - 1)).t(), Xa.submat( 1, 0, TT - 1, 0), temp_vec_col));
-// return(List::create(Z));
-// return(List::create(trans(Z.cols(id_zet(0), id_zet(1) - 1)),
-//                     temp_vec_col,
-//                     (trans(Z.cols(id_zet(0), id_zet(1) - 1)) * temp_vec_col)));
-// return(List::create((Z.col(id_bet(0)), (Xa1.submat(0, 0, 0, TT - 2)).t()),
-//        Z.col(id_bet(1)), (Xa2.submat(0, 0, 0, TT - 2)).t(),
-//        Z.col(id_bet(2)), (Xa3.submat(0, 0, 0, TT - 2)).t(),
-//        Z.col(id_bet(3)), (Xa4.submat(0, 0, 0, TT - 2)).t(),
-//        Z.col(id_bet(4)), (Xa5.submat(0, 0, 0, TT - 2)).t(),
-//        Z.col(id_bet(5)), (Xa6.submat(0, 0, 0, TT - 2)).t()
-//        ));
-// return(List::create(Z));
-// return(List::create(trans(Z.cols(id_bet(0), id_bet(1) - 1)) * Z.cols(id_bet(0), id_bet(1) - 1)/sig_sq_x(0, 1),
-//                     prior_V_xa1, Omega_xa1,
-//        sig_sq_x(0, 1)));
