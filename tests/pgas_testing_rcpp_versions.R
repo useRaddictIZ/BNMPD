43e5634041bc9d2b3f57665b52dd769892ea4909
Ilya Zarubin <zarubin@wiso.uni-koeln.de>
2020-01-27 01:09:50 +0100

    preparing next refactoring part III of cpp code
    - some addition to update rate plotting function made (to account for the fact that
    sometimes, in the cpp functions, state trajectories revert states x num_mcmc to
    num_mcmc x states and vice verca; now the function checks for that and ajdusts the
    plots)
    
    - 4 files for testing created with different pgas and cbpf functions:
      cbpf_c2: R random numbers, long version with inefficient container use
      cbpf_c3: arma random numbers, long version with inefficient container use
      cbpf_c4: R random numbers, but short version with looping over d=1,...,D and
               efficient container use
      cbpf_c5: arma random numbers, but short version with looping over d=1,...,D and
               efficient container use
      filter c2&c3 are used in pgas2_full_rng_R and pgas2_full_rng_arma, rescpecitively
      filter c4&c5 are used in pgas2_full_short_rng_R and pgas2_full_short_rng_arma,
      rescpecitively
    
    - having the above 4 files for 4 use cases, all can be conveniently modified and
    continuously tested via the ./tests/pgas_testing_rcpp.R script!

--- tests/pgas_testing_rcpp_versions.R
+++ tests/pgas_testing_rcpp_versions.R
@@ -1,20 +1,76 @@
-# rm(list = ls())
-# sapply(paste0(getwd(),"/R/", list.files(paste0(getwd(),"/R/"), recursive = TRUE)), source)
-# sapply(paste0(getwd(),"/src/", list.files(paste0(getwd(),"/src/"), recursive = TRUE)), Rcpp::sourceCpp)
-# set.seed(139423) # set.seed(3) #
-# init_at_true <- TRUE
-# source("./tests/02_settings_simulation_data.R")
-# source("./tests/02_settings_simulation_init.R")
-# Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions.cpp")
-# Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions2.cpp")
+rm(list = ls())
+sapply(paste0(getwd(),"/R/", list.files(paste0(getwd(),"/R/"), recursive = TRUE)), source)
+sapply(paste0(getwd(),"/src/", list.files(paste0(getwd(),"/src/"), recursive = TRUE)), Rcpp::sourceCpp)
+set.seed(139423) # set.seed(3) #
+init_at_true <- TRUE
+source("./tests/02_settings_simulation_data.R")
+source("./tests/02_settings_simulation_init.R")
+Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions_rng_R.cpp")
+Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions2_rng_R.cpp")
+Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions_rng_arma.cpp")
+Rcpp::sourceCpp("tests/pgas_testing_rcpp_versions2_rng_arma.cpp")
 # source("tests/pgas_testing_R_versions.R")
-# seed_nr <- 234
+
+seed_nr <- 234
+set.seed(seed_nr)
+par_init_cpp_version <- lapply(par_init, unlist)
+deviate_states_init2 <- as.vector(sapply(as.list(deviate_states_init), rep, times = TT))
+set.seed(seed_nr)
+out1 <- pgas2_full_rng_R(10000, TT, 5, y_t, num_counts,
+                     za1_t, za2_t, za3_t, za4_t, za5_t, za6_t,
+                     c(prior_a, prior_b),
+                     par_init_cpp_version,
+                     deviate_states_init2)
+set.seed(seed_nr)
+test_list_Z <- cbind(za1_t, za2_t, za3_t, za4_t, za5_t, za6_t)
+out2 <- pgas2_full_short_rng_R(10000, TT, 5, y_t, num_counts,
+                            test_list_Z,
+                            c(prior_a, prior_b),
+                            par_init_cpp_version,
+                            deviate_states_init)
+all.equal(out1, out2)
 #
 #
 #
 #
 #
+set.seed(seed_nr)
+out3 <- pgas2_full_rng_arma(10000, TT, 5, y_t, num_counts,
+                            za1_t, za2_t, za3_t, za4_t, za5_t, za6_t,
+                            c(prior_a, prior_b),
+                            par_init_cpp_version,
+                            deviate_states_init2)
+set.seed(seed_nr)
+test_list_Z <- cbind(za1_t, za2_t, za3_t, za4_t, za5_t, za6_t)
+out4 <- pgas2_full_short_rng_arma(10000, TT, 5, y_t, num_counts,
+                                  test_list_Z,
+                                  c(prior_a, prior_b),
+                                  par_init_cpp_version,
+                                  deviate_states_init)
+all.equal(out3, out4)
+# all.equal(pgas2c2, pgas2c[1:6])
+# print(pgas2c)
 # set.seed(seed_nr)
+# pgas2R <- pgas2(N = 10000, MM = num_mcmc, TT = TT,
+#                 y = y_t, num_counts = num_counts,
+#                 Za1 = za1_t, Za2 = za2_t,
+#                 Za4 = za4_t, Za3 = za3_t,
+#                 Za5 = za5_t, Za6 = za6_t,
+#                 priors = c(prior_a, prior_b),
+#                 par_init = par_init,
+#                 traj_init = deviate_states_init,
+#                 filtering = TRUE,
+#                 num_plots_states = 20)
+# # print(pgas2R)
+# all.equal(unname(unlist(pgas2c)), unlist(pgas2R))
+# # all.equal(unlist(pgas2c[1:18]), unlist(pgas2R[1:18]))
+# # max_print <- length(pgas2c)
+# for (i in 7:8) {
+#   print(pgas2c[[i]])
+#   print(pgas2R[[i]])
+# }
+#
+#
 # out_pgas_sim1 <- pgas1(N = 10, MM = num_mcmc, TT = TT,
 #                       y = y_t,
 #                       Za1 = za1_t, Za2 = za2_t,
@@ -38,43 +94,6 @@
 #                       num_plots_states = 20)
 # all.equal(out_pgas_sim1, out_pgas_sim2)
 # set.seed(seed_nr)
-par_init_cpp_version <- lapply(par_init, unlist)
-deviate_states_init2 <- as.vector(sapply(as.list(deviate_states_init), rep, times = TT))
-set.seed(seed_nr)
-pgas2c1 <- pgas2_full(10000, TT, 5, y_t, num_counts,
-                     za1_t, za2_t, za3_t, za4_t, za5_t, za6_t,
-                     c(prior_a, prior_b),
-                     par_init_cpp_version,
-                     deviate_states_init2)
-set.seed(seed_nr)
-test_list_Z <- cbind(za1_t, za2_t, za3_t, za4_t, za5_t, za6_t)
-pgas2c2 <- pgas2_full_short(10000, TT, 5, y_t, num_counts,
-                            test_list_Z,
-                            c(prior_a, prior_b),
-                            par_init_cpp_version,
-                            deviate_states_init)
-all.equal(pgas2c1, pgas2c2)
-# all.equal(pgas2c2, pgas2c[1:6])
-# print(pgas2c)
-# set.seed(seed_nr)
-# pgas2R <- pgas2(N = 10000, MM = num_mcmc, TT = TT,
-#                 y = y_t, num_counts = num_counts,
-#                 Za1 = za1_t, Za2 = za2_t,
-#                 Za4 = za4_t, Za3 = za3_t,
-#                 Za5 = za5_t, Za6 = za6_t,
-#                 priors = c(prior_a, prior_b),
-#                 par_init = par_init,
-#                 traj_init = deviate_states_init,
-#                 filtering = TRUE,
-#                 num_plots_states = 20)
-# # print(pgas2R)
-# all.equal(unname(unlist(pgas2c)), unlist(pgas2R))
-# # all.equal(unlist(pgas2c[1:18]), unlist(pgas2R[1:18]))
-# # max_print <- length(pgas2c)
-# for (i in 7:8) {
-#   print(pgas2c[[i]])
-#   print(pgas2R[[i]])
-# }
 # microbenchmark::microbenchmark(pgas1(N = 5, MM = 10, TT = TT,
 #                                      y = y_t,
 #                                      Za1 = za1_t, Za2 = za2_t,
